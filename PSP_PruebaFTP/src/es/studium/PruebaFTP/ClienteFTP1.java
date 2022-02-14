d32a8e1e18bdd6060aa2668c2f414cfa7a617208
Alvaro Cruz <35642668+alvaroczrm@users.noreply.github.com>
2022-02-14 19:12:51 +0100

    Add files via upload

---
+++ PSP_PruebaFTP/src/es/studium/PruebaFTP/ClienteFTP1.java
@@ -0,0 +1,33 @@
+package es.studium.PruebaFTP;
+
+import java.io.IOException;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+import org.apache.commons.net.ftp.FTPClient;
+
+public class ClienteFTP1 {
+	public static void main(String[] args) {
+		try {
+			FTPClient cliente = new FTPClient();
+			String servFTP = "ftp.rediris.es"; // Servidor FTP
+			System.out.println("Nos conectamos a: " + servFTP);
+			cliente.connect(servFTP);
+// Respuesta del servidor FTP
+			System.out.print(cliente.getReplyString());
+// Código de respuesta
+			int respuesta = cliente.getReplyCode();
+// Comprobación del código de respuesta
+			if (!FTPReply.isPositiveCompletion(respuesta)) {
+				cliente.disconnect();
+				System.out.println("Conexión rechazada: " + respuesta);
+				System.exit(0);
+			} else {
+// Desconexión del servidor FTP
+				cliente.disconnect();
+				System.out.println("Conexión finalizada.");
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
